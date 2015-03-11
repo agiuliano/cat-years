619b7510d20ef2644683cb71252c2b3f4f363c6e
Andrea Giuliano <giulianoand@gmail.com>
2015-03-11 13:55:25 +0100

    add simple logic

--- CatYears/ViewController.swift
+++ CatYears/ViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet var age: UITextField!
+    @IBOutlet var textResult: UILabel!
+    
+    @IBAction func onButtonPressed(sender: AnyObject) {
+        var enteredAge = age.text.toInt()
+        
+        if enteredAge != nil {
+            var catYears = enteredAge! * 7
+            
+            textResult.text = "Your cat is \(catYears) in cat years"
+        } else {
+            textResult.text = "Please enter a number in the box"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
